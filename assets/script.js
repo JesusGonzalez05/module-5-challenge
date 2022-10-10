2a175d0afefa441ad03bebeed858a0c4c1227ec8
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-10-10 17:14:12 -0400

    used moment.js to place the current date in header, began working on local storage.

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// use moment().format() to get the date 
+$("#currentDay").text(moment().format("MMM Do, YYYY"));
+
+// allow time blocks to change color based on current time
+
+// save button adds text to local storage
+var saveButton = $('#saveBtn');
