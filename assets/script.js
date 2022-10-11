45e6cb5afafc1988dbc93592d794e83fe490e606
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-10-11 14:13:07 -0400

    placed get methods to get local storage and display on page in associated time.

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,17 @@
 $("#currentDay").text(moment().format("MMM Do, YYYY"));
 
 // allow time blocks to change color based on current time
+var currentTime = moment().format('HH');
+$('.time').each(function(){
+    var timeListed = $('.time').parent().attr("id");
+    if (currentTime === timeListed) {
+      $(this).addClass("present")
+    }
 
+});
+var hours = $('.time').parent().attr("id");
+console.log(hours)
+if (hours )
 // save button adds time and text to local storage
 var saveButton = $('.saveBtn');
 
@@ -13,3 +23,13 @@ $('.saveBtn').on('click', function () {
 
 localStorage.setItem(time, task);
   });
+// set values from local storage 
+$('#9 .textArea').val(localStorage.getItem('9'));
+$('#10 .textArea').val(localStorage.getItem('10'));
+$('#11 .textArea').val(localStorage.getItem('11'));
+$('#12 .textArea').val(localStorage.getItem('12'));
+$('#13 .textArea').val(localStorage.getItem('13'));
+$('#14 .textArea').val(localStorage.getItem('14'));
+$('#15 .textArea').val(localStorage.getItem('15'));
+$('#16 .textArea').val(localStorage.getItem('16'));
+$('#17 .textArea').val(localStorage.getItem('17'));
