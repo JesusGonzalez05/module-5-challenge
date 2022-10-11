6e5aacfacbe0dab1f8a5e81241d0354276efb3bc
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-10-11 12:08:32 -0400

    implemented the ability to type in an event and store it to local storage via associate time.

--- assets/script.js
+++ assets/script.js
@@ -3,5 +3,13 @@ $("#currentDay").text(moment().format("MMM Do, YYYY"));
 
 // allow time blocks to change color based on current time
 
-// save button adds text to local storage
-var saveButton = $('#saveBtn');
+// save button adds time and text to local storage
+var saveButton = $('.saveBtn');
+
+$('.saveBtn').on('click', function () {
+    var task = $(this).siblings('.textArea').val();
+    var time = $(this).parent().attr("id");
+    console.log(time);
+
+localStorage.setItem(time, task);
+  });
