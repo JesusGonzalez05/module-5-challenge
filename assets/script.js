f83c9254dbcfebc870b69ed15d4456b5e50f8344
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-10-11 21:46:01 -0400

    finsihed remaing code.

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,22 @@
 // use moment().format() to get the date 
 $("#currentDay").text(moment().format("MMM Do, YYYY"));
 
-// allow time blocks to change color based on current time
+// allow time blocks to change color based on current time - HH allows for only hr 
 var currentTime = moment().format('HH');
 
+// create function for color change
 $('.time').each(function(){
   var timeListed = $(this).parent().attr("id");
+  // change the color if its the current time - red
   if (currentTime === timeListed) {
     $(this).addClass("present")
   }
+  // color of all hours in the future green
   else if (currentTime < timeListed) {
     $(this).removeClass("present")
     $(this).addClass("future")
   }
+  // color of all hours in the past green
   if (currentTime > timeListed) {
     $(this).removeClass("future")
     $(this).addClass("past")
@@ -22,6 +26,7 @@ $('.time').each(function(){
 // save button adds time and text to local storage
 var saveButton = $('.saveBtn');
 
+// add the text area to local storage and its hour id
 $('.saveBtn').on('click', function () {
     var task = $(this).siblings('.textArea').val();
     var time = $(this).parent().attr("id");
@@ -29,7 +34,8 @@ $('.saveBtn').on('click', function () {
 
 localStorage.setItem(time, task);
   });
-// set values from local storage 
+
+  // set values from local storage 
 $('#9 .textArea').val(localStorage.getItem('9'));
 $('#10 .textArea').val(localStorage.getItem('10'));
 $('#11 .textArea').val(localStorage.getItem('11'));
